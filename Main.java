8f9cdd4b2b20a59030d444176bbc61e0c9efcc55
hazymi <jyzelsevilla@gmail.com>
2025-08-23 14:14:12 +0000

    SEATWORK SIX

--- Main.java
+++ Main.java
@@ -1,1 +1,15 @@
-
+public class Main {
+public static void main (String[] args) {
+System.out.println("SEATWORKSIX");
+    int dice = 1;
+    
+    while (dice < 6 ){
+     if (dice < 6 ){
+        System.out.println("pogi sir lawrence");
+      }else {
+        System.out.println("tamang tama");
+      }
+      dice = dice + 1;
+    }
+  }
+}
