17727b75db08a91f0c84df456d97ab89e4800135
SCRICOR ZALLY <jyzelsevilla@gmail.com>
2025-08-28 13:48:42 +0000

    SeatworkEight

--- SeatworkEight.java
+++ SeatworkEight.java
@@ -1,1 +1,29 @@
 
+public class SeatworkEight {
+    public static void main(String[] args) {
+    int grade = 3;
+    
+    switch(grade){
+      case 1:
+      System.out.println("Excellent");
+      break;
+      case 2 :
+      System.out.println("great job");
+      break;
+      case 3:
+      System.out.println("well done");
+      break;
+      case 4:
+      System.out.println("you passed");
+      break;
+      case 5:
+      System.out.println("better try next time");
+      break;
+      default:
+      System.out.println("invalid grade");
+      
+    }
+    System.out.println("your grade is"+ grade);
+  }
+}
+
