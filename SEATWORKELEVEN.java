b447225b3f511c5d037ccc3de5b0b93d206bfb4b
SCRICOR ZALLY <jyzelsevilla@gmail.com>
2025-09-09 10:30:41 +0000

    Seatwork

--- SEATWORKELEVEN.java
+++ SEATWORKELEVEN.java
@@ -1,1 +1,9 @@
+public class SEATWORKELEVEN {
+public static void main(String[] args) {
+    System.out.println("SEATWORKELEVENPART1");
+    int [][] mynumbers={ {1,4,2},{3,6,8} };
+    mynumbers[1][2]=9;
+    System.out.println(mynumbers[1][2]);
+  }
+}
 
--- SEATWORKELEVENPARTTWO.java
+++ SEATWORKELEVENPARTTWO.java
@@ -1,1 +1,14 @@
+public class SEATWORKELEVENPARTTWO {
+public static void main(String[] args) {
+   System.out.println("SEATWORKELEVENPART2");
+    int [][] mynumbers={ {1,4,2},{3,6,8,5,2} };
+    for(int row=0; row<mynumbers.length; row++){
+      for (int col=0;col < mynumbers[row].length; col++) {
+        System.out.println("mynumbers["+row+"]" + "["+col+"]="+mynumbers[row][col]);
+      }
+      
+    }
+  }
+}
+
 
